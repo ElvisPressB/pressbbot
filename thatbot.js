8ea2dc1f300dc45fb857bb59ff8c2686efc3e500
Zoneman <thezoneman33@gmail.com>
2018-10-05 09:00:13 -0500

    Fix error handling on the insta command

--- thatbot.js
+++ thatbot.js
@@ -185,13 +185,17 @@ client.on('chat', function(channel, user, message, self) {
 						}
 						break;
 					case "!insta":
-						if(!command.modOnly || (command.modOnly && checkMod(user, channel))) {
-							if (subSeconds(command.cooldown) >= Cooldowns[parsed.command]) {
-								client.say(channel, command.message.replace("$username", user['display-name']));
-								Cooldowns[parsed.command] = new Date();
-							} else {
-								console.log("Insta cooldown not up");
+						try {
+							if(!command.modOnly || (command.modOnly && checkMod(user, channel))) {
+								if (subSeconds(command.cooldown) >= Cooldowns[parsed.command]) {
+									client.say(channel, command.message.replace("$username", user['display-name']));
+									Cooldowns[parsed.command] = new Date();
+								} else {
+									console.log("Insta cooldown not up");
+								}
 							}
+						} catch (err) {
+							const e = debugLevel === 1 ? console.log("Error: " + err.message) : debugLevel === 2 ? console.log("Error: " + err.stack) : null; 
 						}
 						break;
 					case "!hype":
